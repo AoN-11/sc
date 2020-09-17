2a4e3b390f5d125fbbd9533f7819326c20d19a72
Aoi <nakane-ao@g.ecc.u-tokyo.ac.jp>
2020-09-17 10:50:18 +0900

    Initial commit

---
+++ app/src/main/java/events/tgh2020/sc/MainActivity.java
@@ -0,0 +1,14 @@
+package events.tgh2020.sc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
