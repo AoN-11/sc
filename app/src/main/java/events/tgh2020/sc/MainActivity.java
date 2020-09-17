8ccd6025b8e680c66c4d40b6bd981fefc28e436c
Aoi <nakane-ao@g.ecc.u-tokyo.ac.jp>
2020-09-17 14:20:31 +0900

    ボタンの挙動

--- app/src/main/java/events/tgh2020/sc/MainActivity.java
+++ app/src/main/java/events/tgh2020/sc/MainActivity.java
@@ -3,6 +3,7 @@ package events.tgh2020.sc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +17,12 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.button1);
         callInstance = new Call(getApplicationContext());
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                callInstance.play();
+            }
+        });
     }
 }
